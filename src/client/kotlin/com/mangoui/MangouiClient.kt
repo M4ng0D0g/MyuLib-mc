51712bce5343de2ae16a4f7dc9a557f1ef3a5479
M4ng0D0g <chiayu.myu@icloud.com>
2026-03-19 16:02:04 +0800

    initial commit

---
+++ src/client/kotlin/com/mangoui/MangouiClient.kt
@@ -0,0 +1,9 @@
+package com.mangoui
+
+import net.fabricmc.api.ClientModInitializer
+
+object MangouiClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/mangoui/Mangoui.kt
@@ -0,0 +1,15 @@
+package com.mangoui
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Mangoui : ModInitializer {
+    private val logger = LoggerFactory.getLogger("mango-ui")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
